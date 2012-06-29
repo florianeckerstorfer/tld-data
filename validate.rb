4b237a930aa71989b85efdf540a85e1a69cf8132
Florian Eckerstorfer <florian@theroadtojoy.at>
2012-06-29 14:16:33 +0200

    Renamed JSON files.

--- validate.rb
+++ validate.rb
@@ -2,18 +2,18 @@ require 'rubygems'
 require 'json'
 require 'jsonschema'
 
-schema     = File.open("schema.json",      "rb"){|f| JSON.parse(f.read)}
-dataGTlds  = File.open("data/gtlds.json",  "rb"){|f| JSON.parse(f.read)}
-dataCcTlds = File.open("data/cctlds.json", "rb"){|f| JSON.parse(f.read)}
+schema     = File.open("schema.json",  "rb"){|f| JSON.parse(f.read)}
+dataGTlds  = File.open("data/g.json",  "rb"){|f| JSON.parse(f.read)}
+dataCcTlds = File.open("data/cc.json", "rb"){|f| JSON.parse(f.read)}
 
 if JSON::Schema.validate(dataGTlds, schema)
-	puts "gtlds.json is valid.\n"
+	puts "g.json is valid.\n"
 else
-	puts "gtlds.json is NOT valid.\n"
+	puts "g.json is NOT valid.\n"
 end
 
 if JSON::Schema.validate(dataCcTlds, schema)
-	puts "cctlds.json is valid.\n"
+	puts "cc.json is valid.\n"
 else
-	puts "cctlds.json is NOT valid.\n"
+	puts "cc.json is NOT valid.\n"
 end
