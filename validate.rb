1426759e1e41941c555bcf7e7c4286d447742c0b
Florian Eckerstorfer <florian@theroadtojoy.at>
2012-06-29 15:30:11 +0200

    Merged g.json and cc.json and added categories.json.

--- validate.rb
+++ validate.rb
@@ -2,18 +2,11 @@ require 'rubygems'
 require 'json'
 require 'jsonschema'
 
-schema     = File.open("schema.json",  "rb"){|f| JSON.parse(f.read)}
-dataGTlds  = File.open("data/g.json",  "rb"){|f| JSON.parse(f.read)}
-dataCcTlds = File.open("data/cc.json", "rb"){|f| JSON.parse(f.read)}
+schema = File.open("schema.json",    "rb"){|f| JSON.parse(f.read)}
+data   = File.open("data/tlds.json", "rb"){|f| JSON.parse(f.read)}
 
-if JSON::Schema.validate(dataGTlds, schema)
-	puts "g.json is valid.\n"
+if JSON::Schema.validate(data, schema)
+	puts "tlds.json is valid.\n"
 else
-	puts "g.json is NOT valid.\n"
-end
-
-if JSON::Schema.validate(dataCcTlds, schema)
-	puts "cc.json is valid.\n"
-else
-	puts "cc.json is NOT valid.\n"
+	puts "tlds.json is NOT valid.\n"
 end
